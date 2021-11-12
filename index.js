66421dae4fe528636a5585d694e07f2be440fc6c
shoaibakther <shoaibakthershopno@gmail.com>
2021-11-12 23:17:15 +0600

    updeted

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const cors = require('cors');
 // const admin = require("firebase-admin");
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 
 const port = process.env.PORT || 5000;
+
+var admin = require("firebase-admin");
+
+var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(cors());
 app.use(express.json());
@@ -13,12 +23,28 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m7jsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const token = req.headers.authorization.split(' ')[1]
+
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(token);
+      req.decodedEmail = decodedUser.email
+    }
+    catch {
+      
+    }
+  }
+  next();
+}
+
+
 async function run() {
     try {
         await client.connect();
         const database = client.db('sunglassStore');
            const productsCollection = database.collection('products');
-
+          const usersCollection = database.collection('users')
         
         // Get all products 
          app.get('/products', async (req, res) => {
@@ -26,24 +52,75 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products)
          })
+      // Get single api 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await productsCollection.findOne(query)
+            res.json(service)
+        })
         
         // post api 
         app.post('/products', async (req, res) => {
             const service = req.body;
-     
             const result = await productsCollection.insertOne(service)
-            // console.log(result);
             res.json(result)
         })
+      
+      // user post 
+      app.post('/users', async (req, res) => {
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+        console.log(result);
+        res.json(result)
+      })
+      // update users 
+      app.put('/users', async (req, res) => {
+        const user = req.body;
+        const filter = { email: user.email };
+        const options = { upsert: true };
+        const updateDoc = { $set: user };
+        const result = await usersCollection.updateOne(filter, updateDoc, options)
+        res.json(result)
+      })
+
+      // admin email 
+      app.get('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query)
+        let isAdmin = false;
+        if (user?.role === 'admin') {
+          isAdmin = true;
+        }
+        res.json({admin: isAdmin})
+      })
+
+      // make admin 
+      app.put('/users/admin',verifyToken, async (req, res) => {
+        const user = req.body;
+        const requester = req.decodedEmail;
+        if (requester) {
+          const requesterAccount = await usersCollection.findOne({email: requester})
+          if (requesterAccount.role === 'admin') {
+            const filter = { email: user.email };
+        const updateDoc = { $set: {role: 'admin'}}
+        const result = await usersCollection.updateOne(filter, updateDoc)
+        res.json(result);
+          }
+        }
+        else {
+          res.status(403).json({message: 'You have no permit'})
+        }
+      
+      })
+
        
     } finally {
         // await client.close();
     }
 }
 run().catch(console.dir)
-
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
